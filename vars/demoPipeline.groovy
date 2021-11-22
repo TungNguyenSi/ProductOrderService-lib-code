022eea30c07338862244c768adfebd06f216d70d
TungNguyenSi <nstung@IfiSolution.local>
2021-11-22 17:43:04 +0700

    test

--- vars/demoPipeline.groovy
+++ vars/demoPipeline.groovy
@@ -3,13 +3,17 @@ def call(Map args){
     node ("agent1"){
         def docker = args.jsl.com.ifi.jenkins.Docker.new(this)
         def git = args.jsl.com.ifi.jenkins.Git.new(this)
+        stage("pull code") {
+            git.clone("https://github.com/TungNguyenSi/ProductOrderService.git")
+        }
+        stage("build image") {
+            docker.build("gcr.io/jenkins-demo-330307/product-order-service:release-1.0")
+        }
     }
     return this
 }
 
-def buildImage() {
-    git.clone("https://github.com/TungNguyenSi/ProductOrderService.git")
-    docker.build("gcr.io/jenkins-demo-330307/product-order-service:release-1.0")
+//def buildImage() {
 //    node ("agent1") {
 //        stage("pull code") {
 //            git.clone("https://github.com/TungNguyenSi/ProductOrderService.git")
@@ -18,5 +22,5 @@ def buildImage() {
 //            docker.build("gcr.io/jenkins-demo-330307/product-order-service:release-1.0")
 //        }
 //    }
-    return this
-}
+//    return this
+//}
