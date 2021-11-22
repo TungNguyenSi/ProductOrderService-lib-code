8017c75a8b5dcf13564297c626aea4d55db17156
TungNguyenSi <nstung@IfiSolution.local>
2021-11-22 17:45:15 +0700

    test

--- src/com/ifi/jenkins/Git.groovy
+++ src/com/ifi/jenkins/Git.groovy
@@ -15,16 +15,8 @@ class Git implements Serializable{
         script.env.BRANCH_NAME == 'main'
     }
 
-    void clone(def repo){
-        withVault(configuration: [timeout: 60, vaultCredentialId: 'vault-jenkins-approle', vaultUrl: 'http://34.126.70.118:8200'],
-                vaultSecrets: [
-                        [path: 'secrets/creds/Tung.NguyenSi-github', engineVersion: 1, secretValues: [
-                                [envVar: 'githubUsername', vaultKey: 'username'],
-                                [envVar: 'githubPassword', vaultKey: 'password']]
-                        ]
-                ]) {
-            script.sh("curl -u \"${githubUsername}:${githubPassword}\" ${repo}")
-            script.sh("git clone ${repo}")
-        }
+    void clone(def repo, def githubUsername, def githubPassword){
+        script.sh("curl -u \"${githubUsername}:${githubPassword}\" ${repo}")
+        script.sh("git clone ${repo}")
     }
 }
--- vars/demoPipeline.groovy
+++ vars/demoPipeline.groovy
@@ -4,7 +4,16 @@ def call(Map args){
         def docker = args.jsl.com.ifi.jenkins.Docker.new(this)
         def git = args.jsl.com.ifi.jenkins.Git.new(this)
         stage("pull code") {
-            git.clone("https://github.com/TungNguyenSi/ProductOrderService.git")
+            withVault(configuration: [timeout: 60, vaultCredentialId: 'vault-jenkins-approle', vaultUrl: 'http://34.126.70.118:8200'],
+                vaultSecrets: [
+                    [path: 'secrets/creds/Tung.NguyenSi-github', engineVersion: 1, secretValues: [
+                        [envVar: 'githubUsername', vaultKey: 'username'],
+                        [envVar: 'githubPassword', vaultKey: 'password']]
+                    ]
+                ]) {
+                git.clone("https://github.com/TungNguyenSi/ProductOrderService.git", githubUsername, githubPassword)
+            }
+
         }
         stage("build image") {
             docker.build("gcr.io/jenkins-demo-330307/product-order-service:release-1.0")
