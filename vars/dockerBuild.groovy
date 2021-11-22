3bdbfdb79b7099fd853416dd9d3091e33223038b
TungNguyenSi <nstung@IfiSolution.local>
2021-11-22 16:29:17 +0700

    test

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,5 +1,11 @@
 def call(Map args) {
     node {
+        agent {
+            docker {
+                image "nstung219/agent-image:1.2"
+                label "agent1"
+            }
+        }
         def docker = args.jsl.com.ifi.jenkins.Docker.new(this)
         docker.build("test");
     }
