b822c8be65729cd31bd2bdff9bcd2d43269ca371
TungNguyenSi <nstung@IfiSolution.local>
2021-11-23 15:16:47 +0700

    test

--- vars/demoPipeline.groovy
+++ vars/demoPipeline.groovy
@@ -6,24 +6,31 @@ def call(Map args) {
   def gDocker = new Docker()
   def k8s = new K8s()
   node("agent1") {
-//    docker.image('nstung219/agent-image:1.2').inside {
-//      stage("build image") {
-//        gDocker.build("product-order-service:release-1.0")
-//      }
-//      stage("push image") {
-//        gDocker.push("gcr.io/jenkins-demo-330307", "product-order-service:release-1.0")
-//      }
-//    }
-    stage("deploy") {
-      k8s.auth()
-      k8s.createMongoSecrets()
-      k8s.apply("-f mongo-deploy.yaml")
-      k8s.apply("-f product-order-service-deploy.yaml")
-      def mongoVerify = k8s.verifyRunningPods("mongo")
-      def serverVerify = k8s.verifyRunningPods("server")
+    docker.image('nstung219/agent-image:1.2').inside {
+      stage("build image") {
+        gDocker.build("product-order-service:release-1.0")
+      }
+      stage("push image") {
+        gDocker.push("gcr.io/jenkins-demo-330307", "product-order-service:release-1.0")
+      }
+    }
+  }
 
-      if (mongoVerify == false || serverVerify == false){
-        currentBuild.result = "FAILURE"
+  podTemplate(containers: [
+    containerTemplate(name: 'jnlp', image: 'nstung219/k8s-agent:1.5')
+  ]) {
+    node ("kubepod") {
+      stage("deploy") {
+        k8s.auth()
+        k8s.createMongoSecrets()
+        k8s.apply("-f mongo-deploy.yaml")
+        k8s.apply("-f product-order-service-deploy.yaml")
+        def mongoVerify = k8s.verifyRunningPods("mongo")
+        def serverVerify = k8s.verifyRunningPods("server")
+
+        if (mongoVerify == false || serverVerify == false){
+          currentBuild.result = "FAILURE"
+        }
       }
     }
   }
