8124b7aaed965bacbe598ff1bd60a276739a1d99
TungNguyenSi <nstung@IfiSolution.local>
2021-11-25 14:38:44 +0700

    test

--- vars/demoPipeline.groovy
+++ vars/demoPipeline.groovy
@@ -16,8 +16,8 @@ def call() {
 //      }
 //    }
 //  }
-  podTemplate(annotations: [podAnnotation(key: 'vault.hashicorp.com/agent-inject', value: 'true'), podAnnotation(key: 'vault.hashicorp.com/role', value: 'webapp'), podAnnotation(key: 'vault.hashicorp.com/agent-inject-secrets-gcloud.json', value: 'secrets/creds/gcloud-service-account')], cloud: 'kubernetes', label: "kaniko", containers: [containerTemplate(image: 'gcr.io/kaniko-project/executor:latest', name: 'kaniko', workingDir: '/home/jenkins/agent')], serviceAccount: 'vault-auth') {
-    node ("kaniko") {
+  podTemplate(annotations: [podAnnotation(key: 'vault.hashicorp.com/agent-inject', value: 'true'), podAnnotation(key: 'vault.hashicorp.com/role', value: 'webapp'), podAnnotation(key: 'vault.hashicorp.com/agent-inject-secrets-gcloud.json', value: 'secrets/creds/gcloud-service-account')], cloud: 'kubernetes', label: "test", containers: [containerTemplate(image: 'gcr.io/kaniko-project/executor:latest', name: 'kaniko', workingDir: '/home/jenkins/agent')], serviceAccount: 'vault-auth') {
+    node ("test") {
       container("kaniko") {
         checkout scm
         sh '''
