e3d937892f9338b3646660e424b9d515dfadd351
TungNguyenSi <nstung@IfiSolution.local>
2021-11-25 11:24:30 +0700

    test

--- vars/demoPipeline.groovy
+++ vars/demoPipeline.groovy
@@ -3,19 +3,6 @@ import com.ifi.jenkins.Docker
 import com.ifi.jenkins.K8s
 
 def call() {
-  def gDocker = new Docker()
-  def k8s = new K8s()
-//  node("agent1") {
-//    docker.image('nstung219/agent-image:1.2').inside {
-//      stage("build image") {
-//        checkout scm
-//        gDocker.build("product-order-service:release-1.0")
-//      }
-//      stage("push image") {
-//        gDocker.push("gcr.io/jenkins-demo-330307", "product-order-service:release-1.0")
-//      }
-//    }
-//  }
   podTemplate(label: "imageAgent", yaml: """
     apiVersion: v1
     kind: Pod
