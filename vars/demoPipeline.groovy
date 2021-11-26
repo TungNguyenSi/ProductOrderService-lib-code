ae2479efa1b57c2a2e6772406727ec0ae43c327b
TungNguyenSi <nstung@IfiSolution.local>
2021-11-26 11:04:00 +0700

    test

--- vars/demoPipeline.groovy
+++ vars/demoPipeline.groovy
@@ -24,10 +24,12 @@ def call() {
     annotations: [
       podAnnotation(key: 'vault.hashicorp.com/agent-inject', value: 'true'),
       podAnnotation(key: 'vault.hashicorp.com/role', value: 'webapp'),
-      podAnnotation(key: 'vault.hashicorp.com/agent-inject-secret-gcloud.json', value: 'secrets/creds/gcloud-service-account'),
+      podAnnotation(key: 'vault.hashicorp.com/agent-inject-secret-gcloud.json', value: 'secrets/creds/kaniko-gcloud'),
       podAnnotation(key: 'vault.hashicorp.com/agent-inject-template-gcloud.json', value: '| ' +
         '{{- with secret "secrets/creds/gcloud-service-account" -}} ' +
-        '  "{{ $value }}"' +
+        ' {\n' +
+        '  "{{ .Data }}"' +
+        ' \n}' +
         '{{- end }}')
     ],
     cloud: 'kubernetes',
